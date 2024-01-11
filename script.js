42c73dd873bc17c48934610c5569b99cfb05ecb5
afrodoug <douglas.oli.severiano@gmail.com>
2024-01-11 19:00:54 -0300

    Validação de javascript em andamento

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert("teste")
+function selecionaTransportadora1(){
+    const alteraBtn = document.getElementById("selecionar1")
+    alteraBtn.style.background = ("#10B981");
+    alteraBtn.textContent="selecionado"
+    // alert('Botão funcioando');
+}
+function selecionaTransportadora2(){
+    const alteraBtn = document.getElementById("selecionar2")
+    alteraBtn.style.background = "#10B981";
+    alteraBtn.textContent="selecionado"
+    // alert('Botão funcioando');
+}
