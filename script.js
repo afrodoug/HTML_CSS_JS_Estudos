190437946038f0a47b173a1d7bf497b8439dc9ac
afrodoug <douglas.oli.severiano@gmail.com>
2024-01-11 23:12:41 -0300

    Alteração nos botões e funcionalidades da página.

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 function selecionaTransportadora1(){
-    const alteraBtn = document.getElementById("selecionar1")
-    const alteraborda=document.getElementById("trans2")
+    const alteraBtn = document.getElementById("selecionar1");
+    const alteraborda=document.getElementById("trans1");
+    const alteraValorTotal=document.getElementById("valorTotal")
+    alteraborda.style.borderColor=("#492BC4");
     alteraBtn.style.background = ("#10B981");
     alteraBtn.textContent="selecionado"
-    // alert('Botão funcioando');
+    alert('Botão funcioando');
+    alteraValorTotal.innerHTML="R$ 1580,00"
 }
 function selecionaTransportadora2(){
     const alteraBtn = document.getElementById("selecionar2")
