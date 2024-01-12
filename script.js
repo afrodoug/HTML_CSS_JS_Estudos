9e7cec734cbeb59ea4d069a87336e47d199f9e73
afrodoug <douglas.oli.severiano@gmail.com>
2024-01-11 22:37:54 -0300

    Atualizações de estilo, inicio da correção dos botões de adicionar e remover

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function selecionaTransportadora1(){
     const alteraBtn = document.getElementById("selecionar1")
+    const alteraborda=document.getElementById("trans2")
     alteraBtn.style.background = ("#10B981");
     alteraBtn.textContent="selecionado"
     // alert('Botão funcioando');
 }
 function selecionaTransportadora2(){
     const alteraBtn = document.getElementById("selecionar2")
-    alteraBtn.style.background = "#10B981";
+    const alteraborda=document.getElementById("trans2")
+    alteraborda.style.borderColor=("#492BC4");
+    alteraBtn.style.background = ("#10B981");
     alteraBtn.textContent="selecionado"
-    // alert('Botão funcioando');
+    alert('Botão funcioando');
 }
