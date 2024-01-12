3f834472d3087fb8b48ed352a8d2e0d4a07cda48
afrodoug <douglas.oli.severiano@gmail.com>
2024-01-11 23:16:22 -0300

    Alterados valores de valor total de acordo com a transportadora

--- script.js
+++ script.js
@@ -6,13 +6,15 @@ function selecionaTransportadora1(){
     alteraBtn.style.background = ("#10B981");
     alteraBtn.textContent="selecionado"
     alert('Botão funcioando');
-    alteraValorTotal.innerHTML="R$ 1580,00"
+    alteraValorTotal.innerHTML="R$ 1600,00"
 }
 function selecionaTransportadora2(){
     const alteraBtn = document.getElementById("selecionar2")
     const alteraborda=document.getElementById("trans2")
+    const alteraValorTotal=document.getElementById("valorTotal")
     alteraborda.style.borderColor=("#492BC4");
     alteraBtn.style.background = ("#10B981");
     alteraBtn.textContent="selecionado"
+    alteraValorTotal.innerHTML="R$ 1580,00"
     alert('Botão funcioando');
 }
