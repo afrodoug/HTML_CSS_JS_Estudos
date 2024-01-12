1b896e6e24688c075727a4aa0b1dfc9e91ca2a51
afrodoug <douglas.oli.severiano@gmail.com>
2024-01-12 15:19:08 -0300

    Correções da página em geral

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
+var alteraValorTotal=document.getElementById("valorTotal")
+var qtdItens = document.getElementsByClassName("main")
 function selecionaTransportadora1(){
     const alteraBtn = document.getElementById("selecionar1");
     const alteraborda=document.getElementById("trans1");
-    const alteraValorTotal=document.getElementById("valorTotal")
     alteraborda.style.borderColor=("#492BC4");
     alteraBtn.style.background = ("#10B981");
     alteraBtn.textContent="selecionado"
-    alert('Botão funcioando');
     alteraValorTotal.innerHTML="R$ 1600,00"
+    qtdItens.backgroundColor=("black")
 }
 function selecionaTransportadora2(){
     const alteraBtn = document.getElementById("selecionar2")
     const alteraborda=document.getElementById("trans2")
-    const alteraValorTotal=document.getElementById("valorTotal")
+    
     alteraborda.style.borderColor=("#492BC4");
     alteraBtn.style.background = ("#10B981");
     alteraBtn.textContent="selecionado"
     alteraValorTotal.innerHTML="R$ 1580,00"
     alert('Botão funcioando');
+}
+function incrementapdt1(){
+    const alteraQtdCarrinho = document.getElementById("qtdProduto");
+    alteraQtdCarrinho.innerHTML=("2")
 }
