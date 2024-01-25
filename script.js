59a3428814c2e935eb48a64d097cdb4a10f56e6f
afrodoug <douglas.oli.severiano@gmail.com>
2024-01-24 22:49:29 -0300

    Merge branch 'main' of https://github.com/afrodoug/HTML_CSS_JS_Estudos

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+var qtdItens = document.querySelector("#qtdItens");
+// qtdItens.innerHTML=("teste")
+var estoque1 = document.getElementById("estoque1")
+// estoque1.innerHTML=("teste")
+var qtdLikes1 = document.getElementById("qtdLikes1")
+// qtdLikes1.innerHTML = ("teste")
 var alteraValorTotal=document.getElementById("valorTotal")
-var qtdItens = document.getElementsByClassName("main")
+//FAZER FUNCIONAR DAQUI PARA BAIXO
+
+parseFloat(alteraValorTotal)
+parseFloat(estoque1)
+parseFloat(qtdItens)
+console.log((alteraValorTotal) = (estoque1)+(qtdItens))
+
+var qtdItens = document.getElementById("qtdItens")
+let itens = 0
+itens.parseFloat = qtdItens
+// function btnTeste(){
+//     const teste = document.getElementsByClassName("Teste")[0];
+//     teste.classList.add("selecionado")
+// }
 // Altera CSS da transportadora Selecionada
 function selecionaTransportadora1(){
     const alteraBtn = document.getElementById("selecionar1");
@@ -7,19 +26,18 @@ function selecionaTransportadora1(){
     alteraborda.style.borderColor=("#492BC4");
     alteraBtn.style.background = ("#10B981");
     alteraBtn.textContent="selecionado"
-    alteraValorTotal.innerHTML="R$ 1600,00"
-    qtdItens.backgroundColor=("black")
+    // alteraValorTotal.innerHTML="R$ 1600,00"
+    // qtdItens.backgroundColor=("black")
+    alert("botaok")
 }
 // Altera CSS da transportadora Selecionada
 function selecionaTransportadora2(){
     const alteraBtn = document.getElementById("selecionar2")
-    const alteraborda=document.getElementById("trans2")
-    
+    const alteraborda=document.getElementById("trans2")    
     alteraborda.style.borderColor=("#492BC4");
     alteraBtn.style.background = ("#10B981");
     alteraBtn.textContent="selecionado"
-    alteraValorTotal.innerHTML="R$ 1580,00"
-    alert('Botão funcioando');
+    // alert('Botão funcioando');
 }
 // Inicio interage com a quantidade de produtos
 function incrementapdt(){
