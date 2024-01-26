885fcd2dd71f847464f873041ab570cfcf76aaba
afrodoug <douglas.oli.severiano@gmail.com>
2024-01-26 00:27:41 -0300

    fix(produto1)
    Corrigidos calculos do produto1

--- src/script.js
+++ src/script.js
@@ -3,26 +3,27 @@ const alteraBorda1=document.getElementById("trans1");
 const alteraBtn1 = document.getElementById("selecionar1");
 const alteraBorda2=document.getElementById("trans2");
 const alteraBtn2 = document.getElementById("selecionar2");
+const valorProduto2 = 25.00;
+const valorProduto3 = 1500.00;
+const valorCorreios = 40.00;
+const valorTransportadora = 20.00
+var qtdEstoque = 100;
+let valorTotal = 0;
+let valorProduto = 15;
+let itensCarrinho = 0;
+let qtdProduto = 0;
+let carrinho = 0
 
-var qtdItens = document.querySelector("#qtdItens");
-// qtdItens.innerHTML=("teste")
-var estoque1 = document.getElementById("estoque1")
-// estoque1.innerHTML=("teste")
-var qtdLikes1 = document.getElementById("qtdLikes1")
-// qtdLikes1.innerHTML = ("teste")
-var alteraValorTotal=document.getElementById("valorTotal")
-//FAZER FUNCIONAR DAQUI PARA BAIXO
-var qtdItens = document.getElementById("qtdItens")
-let itens = 0
-itens.parseFloat = qtdItens
+carregaValores()
+
+// fim constantes globais
 
 function selecionaTransportadora1(){    
     alteraBtn1.classList.add('btnSelecionado');
     alteraBorda1.classList.add('bordaSelecionada')
     alteraBtn2.classList.remove('btnSelecionado');
     alteraBorda2.classList.remove('bordaSelecionada')
-    alteraBtn.textContent="selecionado"    
-    alteraValorTotal.innerHTML="R$ 1600,00"
+    alteraBtn.textContent="selecionado"
 }
 // Altera CSS da transportadora Selecionada
 function selecionaTransportadora2(){
@@ -30,20 +31,29 @@ function selecionaTransportadora2(){
     alteraBorda1.classList.remove('bordaSelecionada')
     alteraBtn2.classList.add('btnSelecionado');
     alteraBorda2.classList.add('bordaSelecionada')
-    alteraBtn.textContent="selecionado";
-    alteraValorTotal.innerHTML="R$ 1580,00";
-
 }
 // Inicio interage com a quantidade de produtos
 function incrementapdt(){
-        let qtdProduto = document.getElementById("qtdProduto");
-        qtdProduto.innerHTML=Number(qtdProduto.innerHTML)+1   
+    qtdProduto+=1;
+    qtdEstoque-=1;
+    carregaValores()
 }
 
 function decrementapdt(){
-    let qtdProduto = document.getElementById("qtdProduto");
-    qtdProduto.innerHTML=Number(qtdProduto.innerHTML)-1
+    if (qtdProduto === 0) return;
+    qtdProduto-=1;
+    qtdEstoque+=1;
+    carregaValores();
 }
 // Fim interage com a quantidade de produtos selecionados
 
 
+function carregaValores(){
+    document.getElementsByClassName("totalItem")[0].innerHTML=qtdProduto;
+    // document.getElementsByClassName("totalItem")[1].innerHTML=qtdProduto;
+    // document.getElementsByClassName("totalItem")[2].innerHTML=qtdProduto;
+    carrinho = qtdProduto
+    document.getElementById("valorTotal").innerHTML=(`${qtdProduto*valorProduto}`).toLocaleString('pt-BR',{style:'currency',currency:'BRL'})
+    document.getElementById("qtdItens").innerHTML=(carrinho)
+    document.getElementById("estoque1").innerHTML=(qtdEstoque);
+}
