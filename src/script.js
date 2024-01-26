ba12c12d7c4a76f54eededd5c7141030c500188f
afrodoug <douglas.oli.severiano@gmail.com>
2024-01-26 02:52:29 -0300

    fix
    Produto 1 regularizado

--- src/script.js
+++ src/script.js
@@ -5,25 +5,29 @@ const alteraBorda2=document.getElementById("trans2");
 const alteraBtn2 = document.getElementById("selecionar2");
 const valorProduto2 = 25.00;
 const valorProduto3 = 1500.00;
-const valorCorreios = 40.00;
+const valorCorreios = 40;
 const valorTransportadora = 20.00
+let alteraTranportadora=0
 var qtdEstoque = 100;
 let valorTotal = 0;
 let valorProduto = 15;
 let itensCarrinho = 0;
 let qtdProduto = 0;
+let qtdProduto2 = 0;
+let qtdProduto3 = 0;
 let carrinho = 0
+// fim constantes globais
 
 carregaValores()
-
-// fim constantes globais
 
 function selecionaTransportadora1(){    
     alteraBtn1.classList.add('btnSelecionado');
     alteraBorda1.classList.add('bordaSelecionada')
     alteraBtn2.classList.remove('btnSelecionado');
     alteraBorda2.classList.remove('bordaSelecionada')
-    alteraBtn.textContent="selecionado"
+    alteraBtn1.textContent="selecionado";
+    alteraTranportadora=valorCorreios;
+    carregaValores()
 }
 // Altera CSS da transportadora Selecionada
 function selecionaTransportadora2(){
@@ -31,6 +35,8 @@ function selecionaTransportadora2(){
     alteraBorda1.classList.remove('bordaSelecionada')
     alteraBtn2.classList.add('btnSelecionado');
     alteraBorda2.classList.add('bordaSelecionada')
+    alteraTranportadora=valorTransportadora;
+    carregaValores();
 }
 // Inicio interage com a quantidade de produtos
 function incrementapdt(){
@@ -45,15 +51,28 @@ function decrementapdt(){
     qtdEstoque+=1;
     carregaValores();
 }
+
+function incrementapdt2(){
+    qtdProduto2+=1;
+    qtdEstoque2-=1;
+    carregaValores()
+}
+
+function decrementapdt2(){
+    if (qtdProduto2 === 0) return;
+    qtdProduto2-=1;
+    qtdEstoque2+=1;
+    carregaValores();
+}
 // Fim interage com a quantidade de produtos selecionados
 
 
 function carregaValores(){
     document.getElementsByClassName("totalItem")[0].innerHTML=qtdProduto;
-    // document.getElementsByClassName("totalItem")[1].innerHTML=qtdProduto;
-    // document.getElementsByClassName("totalItem")[2].innerHTML=qtdProduto;
+    document.getElementsByClassName("totalItem2")[0].innerHTML=qtdProduto2;
+    // document.getElementsByClassName("totalItem")[2].innerHTML=qtdProduto3;
     carrinho = qtdProduto
-    document.getElementById("valorTotal").innerHTML=(`${qtdProduto*valorProduto}`).toLocaleString('pt-BR',{style:'currency',currency:'BRL'})
+    document.getElementById("valorTotal").innerHTML=(`R$ ${(qtdProduto*valorProduto)+alteraTranportadora},00`).toLocaleString('pt-BR',{style:'currency',currency:'BRL'})
     document.getElementById("qtdItens").innerHTML=(carrinho)
     document.getElementById("estoque1").innerHTML=(qtdEstoque);
 }
